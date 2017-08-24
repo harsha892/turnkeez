f1dcc33f56c5c6cc96d250004659534c91067faf
harsha892 <harsha.balarcsoft@gmail.com>
2017-08-24 12:30:34 +0530

    title updated

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Intent Software Solutions Pvt Ltd</title>
+    <title>Turnkeez.com</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Place favicon.ico in the root directory -->
